635fb08da97e0706220a641d008ebbbacdfc3278
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-02-01 12:43:49 +0400

    needed images aded

--- property-selling/app/layout.jsx
+++ property-selling/app/layout.jsx
@@ -1,5 +1,11 @@
 import "@/assets/styles/globals.css";
 
+export const metadata = {
+  title: "Property Selling",
+  keywords: "property, selling, real estate",
+  description: "Property selling website",
+};
+
 const MainLayout = ({ children }) => {
   return (
     <html>
