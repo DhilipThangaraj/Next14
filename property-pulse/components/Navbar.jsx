1a31000972f83798f687dc6cdbf221259cc3eaf8
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-04-24 21:03:51 +0530

    added window resize listener to handle hamburger menu open with certain view port

--- property-pulse/components/Navbar.jsx
+++ property-pulse/components/Navbar.jsx
@@ -1,12 +1,27 @@
 "use client";
 
+import { useState } from "react";
+
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import { FaGoogle } from "react-icons/fa";
 import logo from "@/assets/images/logo-white.png";
 import profileDefault from "@/assets/images/profile.png";
 
 const Navbar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = useState(false);
+  const pathname = usePathname();
+
+  //Close mobile menu if the viewport size is changed
+
+  if (typeof window !== undefined) {
+    window.addEventListener("resize", () => {
+      setIsMobileMenuOpen(false);
+    });
+  }
+
   return (
     <nav className="bg-blue-700 border-b border-blue-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -18,7 +33,7 @@ const Navbar = () => {
               className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
               aria-controls="mobile-menu"
               aria-expanded="false"
-              onClick={() => {}}
+              onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
             >
               <span className="absolute -inset-0.5"></span>
               <span className="sr-only">Open main menu</span>
@@ -50,7 +65,7 @@ const Navbar = () => {
                 <Link
                   href="/"
                   className={`${
-                    true ? "bg-black" : ""
+                    pathname === "/" ? "bg-black" : ""
                   } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >
                   Home
@@ -58,7 +73,7 @@ const Navbar = () => {
                 <Link
                   href="/properties"
                   className={`${
-                    true === "/properties" ? "bg-black" : ""
+                    pathname === "/properties" ? "bg-black" : ""
                   } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >
                   Properties
@@ -66,7 +81,7 @@ const Navbar = () => {
                 <Link
                   href="/properties/add"
                   className={`${
-                    true === "/properties/add" ? "bg-black" : ""
+                    pathname === "/properties/add" ? "bg-black" : ""
                   } text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2`}
                 >
                   Add Property
@@ -173,6 +188,49 @@ const Navbar = () => {
           </div>
         </div>
       </div>
+      {/* <!-- Mobile menu, show/hide based on menu state. --> */}
+      {isMobileMenuOpen && (
+        <div id="mobile-menu">
+          <div className="space-y-1 px-2 pb-3 pt-2">
+            <Link
+              href="/"
+              className={`${
+                pathname === "/" ? "bg-black" : ""
+              } text-white block rounded-md px-3 py-2 text-base font-medium`}
+            >
+              Home
+            </Link>
+            <Link
+              href="/properties"
+              className={`${
+                pathname === "/properties" ? "bg-black" : ""
+              } text-white block rounded-md px-3 py-2 text-base font-medium`}
+            >
+              Properties
+            </Link>
+            <Link
+              href="/properties/add"
+              className={`${
+                pathname === "/properties/add" ? "bg-black" : ""
+              } text-white block rounded-md px-3 py-2 text-base font-medium`}
+            >
+              Add Property
+            </Link>
+            <div className="block md:ml-6">
+              <div className="flex items-center">
+                <button
+                  key={""}
+                  onClick={() => {}}
+                  className="flex items-center text-white bg-gray-700 hover:bg-gray-900 hover:text-white rounded-md px-3 py-2 my-3"
+                >
+                  <FaGoogle className="text-white mr-2" />
+                  <span>Login or Register</span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
