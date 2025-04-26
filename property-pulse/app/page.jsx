34b9752506cabce22448544b48310ba046efbcba
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-04-26 10:10:36 +0530

    card details page created

--- property-pulse/app/page.jsx
+++ property-pulse/app/page.jsx
@@ -1,19 +1,13 @@
-import Link from "next/link";
+import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
+import InfoBoxes from "@/components/InfoBoxes";
 
 export default function HomePage() {
   return (
-    <div className="text-2xl">
-      <h1 className="text-3xl">Welcome</h1>
-      <Link
-        href={{
-          pathname: "/properties",
-          query: {
-            name: "test",
-          },
-        }}
-      >
-        Go to properties
-      </Link>
-    </div>
+    <>
+      <Hero />
+      <InfoBoxes />
+      <HomeProperties />
+    </>
   );
 }
