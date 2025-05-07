6f3b9c0e14323f0022ec409ed99875b746bb8453
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-05-07 07:45:55 +0530

    real routes added for get

--- property-pulse/app/api/properties/route.js
+++ property-pulse/app/api/properties/route.js
@@ -1,12 +1,14 @@
-export const GET = () => {
-  return new Response(
-    JSON.stringify(
-      {
-        message: "Hello World",
-      },
-      {
+import connectDB from "@/config/database";
+import Property from "@/models/Property";
+
+export const GET = async () => {
+  try {
+    await connectDB();
+    const properties = await Property.find();
+    return new Response(
+      JSON.stringify(properties, {
         status: 200,
-      }
-    )
-  );
+      })
+    );
+  } catch (err) {}
 };
