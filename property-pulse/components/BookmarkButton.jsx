d20a14290630927f9ca959a4d46396c4f5c793cf
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-07-01 10:01:39 +0530

    bookmark added and removed button implemented and with bookmark action implemented

--- property-pulse/components/BookmarkButton.jsx
+++ property-pulse/components/BookmarkButton.jsx
@@ -1,40 +1,60 @@
 "use client";
 
 import bookmarkProperty from "@/app/actions/bookmarkProperty";
+import checkBookmarkStatus from "@/app/actions/checkBookmarkStatus";
 import { useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
-useSession;
-toast;
-bookmarkProperty;
 
 const { FaBookmark } = require("react-icons/fa");
 
 const BookmarkButton = ({ property }) => {
   const { data: session } = useSession();
-  const userid = session?.user?.id;
+  const userId = session?.user?.id;
+
+  const [isBookmarked, setIsBookmarked] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!userId) {
+      setLoading(false);
+      return;
+    }
+
+    checkBookmarkStatus(property.id).then((res) => {
+      if (res.error) toast.error(res.error);
+      if (res.isBookmarked) setIsBookmarked(res.isBookmarked);
+      setLoading(false);
+    });
+  }, [property._id, userId, checkBookmarkStatus]);
 
   const handleClick = async () => {
-    if (!userid) {
+    if (!userId) {
       toast.error("You need to be signed in to bookmark a listing");
       return;
     }
 
     bookmarkProperty(property._id).then((res) => {
       if (res.error) return toast.error(res.error);
+      setIsBookmarked(res.isBookmarked);
       toast.success(res.message);
     });
   };
 
-  return (
-    <div className="flex flex-col gap-2">
-      <button
-        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
-        onClick={handleClick}
-      >
-        <FaBookmark className="mr-2" />
-        Bookmark Property
-      </button>
-    </div>
+  return isBookmarked ? (
+    <button
+      onClick={handleClick}
+      className="bg-red-500 hover:bg-red-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Remove Bookmark
+    </button>
+  ) : (
+    <button
+      onClick={handleClick}
+      className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+    >
+      <FaBookmark className="mr-2" /> Bookmark Property
+    </button>
   );
 };
 
