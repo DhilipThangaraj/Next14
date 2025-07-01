7c6c25f72fb10631f0b270bc395bab8c81a7b05c
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-07-01 09:22:55 +0530

    bookmark action got added

---
+++ property-pulse/app/actions/bookmarkProperty.js
@@ -0,0 +1,48 @@
+"use server";
+
+import cloudinary from "@/config/cloudinary";
+import connectDB from "@/config/database";
+import User from "@/models/User";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { revalidatePath } from "next/cache";
+
+async function bookmarkProperty(propertyId) {
+  await connectDB();
+
+  const sessionUser = await getSessionUser();
+
+  if (!sessionUser || !sessionUser.userId) {
+    throw new Error("User ID is required");
+  }
+
+  const { userId } = sessionUser;
+
+  const user = await User.findById(userId);
+
+  const isBookmarked = user.bookmarks.includes(propertyId);
+
+  let message;
+
+  if (isBookmarked) {
+    //if already book marked then remove
+    user.bookmarks.pull(propertyId);
+    message = "Bookmark Removed";
+    isBookmarked = false;
+  } else {
+    //if not bookmarked. then add
+    user.bookmarks.push(propertyId);
+    message = "Bookmark Added";
+    isBookmarked = true;
+  }
+
+  await user.save();
+
+  revalidatePath("/properties/saved", "page");
+
+  return {
+    message,
+    isBookmarked,
+  };
+}
+
+export default bookmarkProperty;
