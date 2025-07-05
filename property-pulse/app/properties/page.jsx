94b132e0ad0accd789cc71b454fbd883fd485039
dhilipkumar thangaraj <dhilipthangaraj777@gmail.com>
2025-07-05 11:46:04 +0530

    pagination added

--- property-pulse/app/properties/page.jsx
+++ property-pulse/app/properties/page.jsx
@@ -1,10 +1,17 @@
+import Pagination from "@/components/Pagination";
 import PropertyCard from "@/components/PropertyCard";
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
 
-const PropertyPage = async () => {
+const PropertyPage = async ({ searchParams: { page = 1, pageSize = 9 } }) => {
   await connectDB();
-  const properties = await Property.find({}).lean();
+  const skip = (page - 1) * pageSize;
+
+  const total = await Property.countDocuments({});
+
+  const properties = await Property.find({}).skip(skip).limit(pageSize);
+
+  const showPagination = total > pageSize;
 
   return (
     <section className="px-4 py-6">
@@ -19,6 +26,13 @@ const PropertyPage = async () => {
           </div>
         )}
       </div>
+      {showPagination && (
+        <Pagination
+          page={parseInt(page)}
+          pageSize={parseInt(pageSize)}
+          totalItems={total}
+        />
+      )}
     </section>
   );
 };
